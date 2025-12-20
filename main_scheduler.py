b4219f8ae69e3939fe36152d96d4b7ac8791154a
priyanshuutomar <tomarpriyanshu506@gmail.com>
2025-12-20 12:27:33 +0530

    Update main_scheduler.py

--- main_scheduler.py
+++ main_scheduler.py
@@ -2,40 +2,13 @@ from core_fcfs import fcfs
 from core_priority import priority_scheduling
 from core_rr import round_robin
 
-print("CPU SCHEDULING SIMULATOR")
-print("1. FCFS")
-print("2. Priority Scheduling")
-print("3. Round Robin")
+def run_all(processes_basic, processes_priority, tq):
 
-choice = int(input("Enter your choice: "))
+    print("===== FCFS =====")
+    fcfs(processes_basic)
 
-n = int(input("Enter number of processes: "))
-processes = []
+    print("\n===== PRIORITY =====")
+    priority_scheduling(processes_priority)
 
-for i in range(n):
-    print(f"\nProcess P{i+1}:")
-    at = int(input(f"Enter arrival time for P{i+1}: "))
-    bt = int(input(f"Enter burst time for P{i+1}: "))
-
-    if choice == 2:
-        pr = int(input(f"Enter priority for P{i+1}: "))
-        processes.append([f"P{i+1}", at, bt, pr])
-    else:
-        processes.append([f"P{i+1}", at, bt])
-
-if choice == 3:
-    tq = int(input("Enter Time Quantum: "))
-
-print("\n--- OUTPUT ---")
-
-if choice == 1:
-    fcfs(processes)
-
-elif choice == 2:
-    priority_scheduling(processes)
-
-elif choice == 3:
-    round_robin(processes, tq)
-
-else:
-    print("Invalid choice!")
+    print("\n===== ROUND ROBIN =====")
+    round_robin(processes_basic, tq)
